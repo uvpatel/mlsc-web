97d5b8abeb9712d8e04e9631f9380a723cd1a12e
uvpatel <uvpatel7271@gmail.com>
2025-12-13 01:26:29 +0530

    Final resolve

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com"], // allow Unsplash
+  },
 };
 
 export default nextConfig;
